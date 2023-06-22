c01765d315e855189549323439430ff9a0c1fefa
Ldi569321 <dragon569321@naver.com>
2023-06-22 15:34:51 +0900

    put & del

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -2,8 +2,10 @@ package com.example.accessingdatamysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +28,23 @@ public class MainController {
 		n.setEmail(email);
 		userRepository.save(n);
 		return "Saved";
+	}
+	@PutMapping(path="/user")
+	public @ResponseBody String updateUser (@RequestParam int id, @RequestParam String name, @RequestParam String email) {
+		User n = new User();
+		n.setId(id);
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Updated";
+	}
+
+	@DeleteMapping(path="/user")
+	public @ResponseBody String deleteUser (@RequestParam int id) {
+		User n = new User();
+		n.setId(id);
+		userRepository.delete(n);
+		return "Deleted";
 	}
 
 	@GetMapping(path="/users")
